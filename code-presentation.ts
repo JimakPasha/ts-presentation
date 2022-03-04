8c57d717e3bdea247a32da4955ef0d5ec3728b6f
pavel <pavel@pavel.com>
2022-03-04 18:02:27 +0300

    feat: add files

---
+++ code-presentation.ts
@@ -0,0 +1,256 @@
+// https://www.typescriptlang.org/play?#code/PTAEFpK6dv4YpzECgQRVxoCqA5ASQHl9QAVATQAUBRTbRmdMJt9jrVb7jARgCUqADYBTAC6gAlgDsADgFdxALlABncQCdZAc1AAfUDIUBbAEajNoALyg+AblSzFk2wHIzAQ01vHzpTZ2jhj+rqBaCqKOPCygAExCYpImAPYKaqK0AG6iMiqgbgDGwlKFANZuBgUAJmbFpRVVbqnpogpylYbNaRnVKQDuMr6oLRnZuWFFJeXDMbEAzEIAZgoyheJSKTKgJgrCG3LCAJ4AQkfk-SmgABTGJqoa2jJ6hncWmgKqb5agAN6ooGkSxu4iOclEKWBdxs1ncj10bgEfwBgNR4gAFpoBqBaJosZprm4AMKeGQAImSewOx3UWgRAkcgIAvijNBIFJpttCAFTxRwskZUqSHU7nS7XACsDLmsQAhPKACxCDCADBBAIIggH4QQB8IIBuEEAMiCAdhBAMIggFYQQA8IKADYAGEEAkiCADhBAEIgRtADrt2sATCDSGQAOlAgHQQW2Oq0ut3B2SgQAsICbAJwgaod7tNDtAgC4QK3J0Bqo2ANhArYA5EBtJpNoHdgGkQQACIFasybALwgdqLEZ1gHkQEMe71OPKWJaeQqiUCEXAZKz-VHSaoPOnPRmomSeEyicdPHRTwGeHTzoymd787iycSd7u9-uDuLIkdSMe0xfL9Qc-Cz9fwyeoAWFLYaUDCFI6Ac-WzXVqaKoR4-IYwGaAkqhZCk542AAfKeqJSMC1xkjOc5kl6oAAUiw4jqir4yGoKRiN6n46P+g7emhogMiOKLMvR4SYtiuL4oSMhXNRiLbnMGBSu2e6aF2PagNQwieEcPy4YC1ELro16ruu3yaDxu77iJAAiX4IUYDSyZe8n0YpXybpYPGguCoBEikJipDI5Bgr2thiRJIGgFpS4yhgnA+b5PmxH5yB9vg5C0AASgAyrQRLkCQZBUHQDCBcw3nJWl6WwDKalCQefYAILVCYsh8Dpo5yU+I76Y+nkjsZG7mGZz4Ceph4FUVMgntJZUGRVqJqLe97lZ5L5vpIZFteGf6eIVshARNMglQAZPlM0dZ8oBQTB1jwV1BFESRZHXNN7XeuetGok1IhfvN1y-OeqhuBFagANLVAAnPMADsABSbgADRGINBQnCkZj-Temh3nOD0ALLeGIRzg3V8wAGxMkIvGsBl2PpQFOMIKAABqxCEBpSX4ylWMU9T7AyisawbFsoBLCkKR8NcOGsuynI8TE9PrJs2wsykcQcwhe3EaIpFfoSAASojCJ+sqIpdPCpTTGvYHjmuQKA+C0IT4XkzT2s62b8AyntkhdlIwiBNcJhqDoQ1IttpUYli-RGKIXusSkBKOzoQgCjEVteksshSHu9uuztjH9Oitu9tcEQ0aVI4SwdMtuBxrN5-nKTysr0ojiyIdq1T5tV9ApvU6AeX4JQAMEM9+DEAA6mQGu19XVeW6NoByN4GT22oLuqKsZQcYMcEIWy4gcts30RaQ3pD5oGTXGo0rl9w-OM9sXgEp4qikkcnMrt6Zgc5d++C8zosn1hMhTwMMgX6AnhXxzvMV8bvdmx7hlUAgAiEBVIAfBAVR+hVIAAhBADEIIAVBBAA4IKAGBCDAAoIIABBBABYIIAQhB-7JSAQAzWvAwAWV7PgPY4kzBiAADzkHgrYYwisqjkHsKAUhoAw6Rw2J4YQEVxCeGjrYf4GBKpAzcNQTwORhD-ViLVNcJkWGeDUHrKhngaGiFocpWCf15GojkOiFI4gUgPTkRgAUGByGgAEUI0QDlLK2HIZCL0UcpB8NsXuYIZDHL1wFlsBxTkbESChqIPK-jtiGAihIPKa5wkH2iBgMObJqgKB7FYP8Gg7GqE8fY3xtgeHuP4YIvcANuwH1UPEwWgT1q5MCbPURYARxqH6FHQo6IbjlMFt6chnMxF4UBIUFRvYqlvkCd6KK5AAD6+A8ow1oMofRAyRzz0Xn8b0GysmlMBtDT+ES14SU-NNJk3jlmDOGX4g+ahxmTKmXlAA4gspZZzQCrM5OszZJTRBlMUXsg+ByjhHOqCc55I5qiiC7FSRZ-SXmvO5tsLZURnksgsZw3IphLmC2ub4xpeFbmzPmYER6tBpkEtoOYppqJbkPPoO4aljy3CxEsWAbKwlezRPEKE0Z2xcUjnIZUiJ2LwQTJJTMuZtBTkjiHoClI00hqnOZV6QSbLgniFiWEiJyIYXhEcgKq5NzRU0slQYw5sqLzKQVZwsOGROX3ntlVCcQcckhPvNy2et1+WYrGY5EVpLxUA2lUCr4950anOtTEtc9s6rKVqRGjVB93W-E9dyoVUt6W0ADaauVn81yhp4NYyhisNF0IYYEZhdtDBsI4agbhMg3EeK+YELq+lJHSIVnIhRSkqGf1UYW6hdCdF6KlUYkxZi9ECmsXU-JOrwQuMKQ2uxjhrEpvqbYMK8L6m0OccCG1oT4KGHXQvTkm7t3qDjbBaINaB4pLSb+G4iKclfNXa43hxS7FlIiXqrFNSGmMRaW0jpR19m9PTvhC5ZJ8XirJIs2FgI3k8o2d6RFAN9JdK2ACoFIKzlDJHhBw1jzoOMTOfBj5SGvk-PXGhn0gazVYeWeCyF+wYOwZI4i68ZduDhttXOe1QNqqxxuEm3VBRIPzPBjR7N1EmQ9q4aNaUXH1VRt+TGxNnriXTJpeJrNF5FLSZUbJwi4hpRAA
+
+// ----------------------------------------------------------------------------------------
+// -------------------------------------- UNION TYPE --------------------------------------
+// ----------------------------------------------------------------------------------------
+
+
+
+
+// 1)
+let input: string | number = 1;
+input = 'bar';
+input = 1;
+// input = true;
+
+
+
+
+// 2)
+let mouseEvent: 'click' | 'dbclick' | 'mouseup' | 'mousedown';
+mouseEvent = 'click';
+
+
+
+
+
+// 3)
+function multiplyByTwo (num: string | number): number {
+  if (typeof num === 'string') {
+      throw Error('Can"t multiply string');
+  }
+  return num * 2;
+}
+
+multiplyByTwo(5);
+
+
+
+
+
+// !!!4)
+// Используем защиту типов in. Защита типа in действует как сужающее выражение для типов.
+interface IUser {
+    id: string;
+    name: string;
+    age: number;
+}
+
+interface IUser2 {
+    id: string;
+    surName: string;
+}
+
+const logUser = (user: IUser | IUser2): void => {
+    if ("name" in user) {
+        console.log(user.name);    
+    }
+    throw Error('no name');
+}
+
+
+
+
+
+// 5)
+interface Player {
+    name: string;
+    age: number;
+}
+
+interface Dog {
+   nickname: string;
+   age: number;
+}
+
+type CommonType = Player | Dog;
+
+
+
+
+
+// ----------------------------------------------------------------------------------------
+// ------------------------------------ INTERSECTION TYPE ---------------------------------
+// ----------------------------------------------------------------------------------------
+
+
+
+
+
+interface IAdmin1 {
+    id: string;
+    name: string;
+    age: number;
+}
+interface IAdmin2 {
+    id: string;
+    surName: string;
+}
+
+const logAdmin = (admin: IAdmin1 & IAdmin2): void => {
+    console.log(admin.id);    
+}
+
+logAdmin({id: 'SsKd937J', name: 'Bob', surName: 'Marley', age: 36})
+
+
+
+
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------- VOID -----------------------------------------
+// ----------------------------------------------------------------------------------------
+
+
+
+
+
+function foo1() {
+  return;
+}
+
+
+
+
+
+function foo2() {
+  console.log('Hello!')
+}
+
+
+
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------- NEVER ----------------------------------------
+// ----------------------------------------------------------------------------------------
+
+
+
+
+
+const fail = (msg: string) => {
+    throw new Error(msg)
+}
+
+
+
+
+
+const infinite = () => {
+    while (true) {
+        console.log('nooooooooooo!!!');
+    }
+}
+
+
+
+
+
+// ----------------------------------------------------------------------------------------
+// -------------------------------------- ANY, UNKNOWN ------------------------------------
+// ----------------------------------------------------------------------------------------
+
+
+
+
+
+const parse = (s: string): unknown => {
+  return JSON.parse(s);
+}
+
+
+
+
+
+function bar(a: any) {
+  a.b()
+}
+
+// function f2(a: unknown) {
+//   a.b();
+// }
+
+
+
+
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------- ТИПИЗАИРУЕМ РЕДАКС ---------------------------------
+// ----------------------------------------------------------------------------------------
+
+// type Nullable<T> = null | T; 
+
+// const initialState = {
+//     name: 'Pavel',
+//     age: null as Nullable<number>,
+//     photo: '',
+// }
+
+// type StateType = typeof initialState;
+// type ActionType = SetNameAction | SetAgeAction;
+
+// const reducer = (state: StateType = initialState, action: ActionType): StateType => {
+//     switch (action.type) {
+//         case ActionsType.SET_NAME:
+//             return {...state, name: action.payload};
+//         case ActionsType.SET_AGE:
+//             return {...state, age: action.payload};
+//         default:
+//             return state;
+//     }
+// }
+
+// enum ActionsType {
+//     SET_NAME = 'SET_NAME',
+//     SET_AGE = 'SET_AGE'
+// }
+
+// interface SetNameAction {
+//     type: ActionsType.SET_NAME;
+//     payload: string;
+// }
+
+// interface SetAgeAction {
+//     type: ActionsType.SET_AGE;
+//     payload: number;
+// }
+
+// const setName = (name: string): SetNameAction => ({type: ActionsType.SET_NAME, payload: name});
+// const setAge = (age: number): SetAgeAction => ({type: ActionsType.SET_AGE, payload: age});
+
+
+type Nullable<T> = null | T; 
+
+const initialState = {
+    name: 'Pavel',
+    age: null as Nullable<number>,
+    photo: '',
+}
+
+type StateType = typeof initialState;
+type ActionsType = ReturnType <typeof setName> | ReturnType <typeof setAge>;
+
+
+const reducer = (state: StateType = initialState, action: ActionsType) => {
+    switch (action.type) {
+        case "SET_NAME":
+            return {...state, name: action.payload};
+        case "SET_AGE":
+            return {...state, age: action.payload};
+        default:
+            return state;
+    }
+}
+
+const setName = (name: string) => ({type: 'SET_NAME', payload: name} as const);
+const setAge = (age: number) => ({type: 'SET_AGE', payload: age} as const);
