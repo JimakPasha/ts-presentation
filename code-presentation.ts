702002cc03358c176896c0a95d0020466d926a44
jimak <jiamak-pg@yandex.by>
2022-03-12 14:39:31 +0300

    feat: add code-txt

--- code-presentation.ts
+++ code-presentation.ts
@@ -1,4 +1,4 @@
-// https://www.typescriptlang.org/play?#code/PTAEFpK6dv4YpzECgQRVxoCqA5ASQHl9QAVATQAUBRTbRmdMJt9jrVb7jARgCUqADYBTAC6gAlgDsADgFdxALlABncQCdZAc1AAfUDIUBbAEajNoALyg+AblSzFk2wHIzAQ01vHzpTZ2jhj+rqBaCqKOPCygAExCYpImAPYKaqK0AG6iMiqgbgDGwlKFANZuBgUAJmbFpRVVbqnpogpylYbNaRnVKQDuMr6oLRnZuWFFJeXDMbEAzEIAZgoyheJSKTKgJgrCG3LCAJ4AQkfk-SmgABTGJqoa2jJ6hncWmgKqb5agAN6ooGkSxu4iOclEKWBdxs1ncj10bgEfwBgNR4gAFpoBqBaJosZprm4AMKeGQAImSewOx3UWgRAkcgIAvijNBIFJpttCAFTxRwskZUqSHU7nS7XACsDLmsQAhPKACxCDCADBBAIIggH4QQB8IIBuEEAMiCAdhBAMIggFYQQA8IKADYAGEEAkiCADhBAEIgRtADrt2sATCDSGQAOlAgHQQW2Oq0ut3B2SgQAsICbAJwgaod7tNDtAgC4QK3J0Bqo2ANhArYA5EBtJpNoHdgGkQQACIFasybALwgdqLEZ1gHkQEMe71OPKWJaeQqiUCEXAZKz-VHSaoPOnPRmomSeEyicdPHRTwGeHTzoymd787iycSd7u9-uDuLIkdSMe0xfL9Qc-Cz9fwyeoAWFLYaUDCFI6Ac-WzXVqaKoR4-IYwGaAkqhZCk542AAfKeqJSMC1xkjOc5kl6oAAUiw4jqir4yGoKRiN6n46P+g7emhogMiOKLMvR4SYtiuL4oSMhXNRiLbnMGBSu2e6aF2PagNQwieEcPy4YC1ELro16ruu3yaDxu77iJAAiX4IUYDSyZe8n0YpXybpYPGguCoBEikJipDI5Bgr2thiRJIGgFpS4yhgnA+b5PmxH5yB9vg5C0AASgAyrQRLkCQZBUHQDCBcw3nJWl6WwDKalCQefYAILVCYsh8Dpo5yU+I76Y+nkjsZG7mGZz4Ceph4FUVMgntJZUGRVqJqLe97lZ5L5vpIZFteGf6eIVshARNMglQAZPlM0dZ8oBQTB1jwV1BFESRZHXNN7XeuetGok1IhfvN1y-OeqhuBFagANLVAAnPMADsABSbgADRGINBQnCkZj-Temh3nOD0ALLeGIRzg3V8wAGxMkIvGsBl2PpQFOMIKAABqxCEBpSX4ylWMU9T7AyisawbFsoBLCkKR8NcOGsuynI8TE9PrJs2wsykcQcwhe3EaIpFfoSAASojCJ+sqIpdPCpTTGvYHjmuQKA+C0IT4XkzT2s62b8AyntkhdlIwiBNcJhqDoQ1IttpUYli-RGKIXusSkBKOzoQgCjEVteksshSHu9uuztjH9Oitu9tcEQ0aVI4SwdMtuBxrN5-nKTysr0ojiyIdq1T5tV9ApvU6AeX4JQAMEM9+DEAA6mQGu19XVeW6NoByN4GT22oLuqKsZQcYMcEIWy4gcts30RaQ3pD5oGTXGo0rl9w-OM9sXgEp4qikkcnMrt6Zgc5d++C8zosn1hMhTwMMgX6AnhXxzvMV8bvdmx7hlUAgAiEBVIAfBAVR+hVIAAhBADEIIAVBBAA4IKAGBCDAAoIIABBBABYIIAQhB-7JSAQAzWvAwAWV7PgPY4kzBiAADzkHgrYYwisqjkHsKAUhoAw6Rw2J4YQEVxCeGjrYf4GBKpAzcNQTwORhD-ViLVNcJkWGeDUHrKhngaGiFocpWCf15GojkOiFI4gUgPTkRgAUGByGgAEUI0QDlLK2HIZCL0UcpB8NsXuYIZDHL1wFlsBxTkbESChqIPK-jtiGAihIPKa5wkH2iBgMObJqgKB7FYP8Gg7GqE8fY3xtgeHuP4YIvcANuwH1UPEwWgT1q5MCbPURYARxqH6FHQo6IbjlMFt6chnMxF4UBIUFRvYqlvkCd6KK5AAD6+A8ow1oMofRAyRzz0Xn8b0GysmlMBtDT+ES14SU-NNJk3jlmDOGX4g+ahxmTKmXlAA4gspZZzQCrM5OszZJTRBlMUXsg+ByjhHOqCc55I5qiiC7FSRZ-SXmvO5tsLZURnksgsZw3IphLmC2ub4xpeFbmzPmYER6tBpkEtoOYppqJbkPPoO4aljy3CxEsWAbKwlezRPEKE0Z2xcUjnIZUiJ2LwQTJJTMuZtBTkjiHoClI00hqnOZV6QSbLgniFiWEiJyIYXhEcgKq5NzRU0slQYw5sqLzKQVZwsOGROX3ntlVCcQcckhPvNy2et1+WYrGY5EVpLxUA2lUCr4950anOtTEtc9s6rKVqRGjVB93W-E9dyoVUt6W0ADaauVn81yhp4NYyhisNF0IYYEZhdtDBsI4agbhMg3EeK+YELq+lJHSIVnIhRSkqGf1UYW6hdCdF6KlUYkxZi9ECmsXU-JOrwQuMKQ2uxjhrEpvqbYMK8L6m0OccCG1oT4KGHXQvTkm7t3qDjbBaINaB4pLSb+G4iKclfNXa43hxS7FlIiXqrFNSGmMRaW0jpR19m9PTvhC5ZJ8XirJIs2FgI3k8o2d6RFAN9JdK2ACoFIKzlDJHhBw1jzoOMTOfBj5SGvk-PXGhn0gazVYeWeCyF+wYOwZI4i68ZduDhttXOe1QNqqxxuEm3VBRIPzPBjR7N1EmQ9q4aNaUXH1VRt+TGxNnriXTJpeJrNF5FLSZUbJwi4hpRAA
+// https://www.typescriptlang.org/play?#code/PTAEFpK6dv4YpzECgQRVxoCqA5ASQHl9QAVATQAUBRTbRmdMJt9jrVb7jARgCUqADYBTAC6gAlgDsADgFdxALlABncQCdZAc1AAfUDIUBbAEajNoALyg+AblSzFk2wHIzAQ01vHzpTZ2jhj+rqBaCqKOPCygAExCYpImAPYKaqK0AG6iMiqgbgDGwlKFANZuBgUAJmbFpRVVbqnpogpylYbNaRnVKQDuMr6oLRnZuWFFJeXDMbEAzEIAZgoyheJSKTKgJgrCG3LCAJ4AQkfk-SmgABTGJqoa2jJ6hncWmgKqb5agAN6ooGkSxu4iOclEKWBdxs1ncj10bgEfwBgNR4gAFpoBqBaJosZprm4AMKeGQAImSewOx3UWgRAkcgIAvijNBIFJpttCAFTxRwskZUqSHU7nS7XACsDLmsQAhPKACxCDCADBBAIIggH4QQB8IIBuEEAMiCAdhBAMIggFYQQA8IKADYAGEEAkiCADhBAEIgRtADrt2sATCDSGQAOlAgHQQW2Oq0ut3B2SgQAsICbAJwgaod7tNDtAgC4QK3J0Bqo2ANhArYA5EBtJpNoHdgGkQQACIFasybALwgdqLEZ1gHkQEMe71OPKWJaeQqiUCEXAZKz-VHSaoPOnPRmomSeEyicdPHRTwGeHTzoymd787iycSd7u9-uDuLIkdSMe0xfL9Qc-Cz9fwyeoAWFLYaUDCFI6Ac-WzXVqaKoR4-IYwGaAkqhZCk542AAfKeqJSMC1xkjOc5kl6oAAUiw4jqir4yGoKRiN6n46P+g7emhogMiOKLMvR4SYtiuL4oSMhXNRiLbnMGBSu2e6aF2PagNQwieEcPy4YC1ELro16ruu3yaDxu77iJAAiX4IUYDSyZe8n0YpXybpYPGguCoBEikJipDI5Bgr2thiRJIGgFpS4yhgnA+b5PmxH5yB9vg5C0AASgAyrQRLkCQZBUHQDCBcw3nJWl6WwDKalCQefYAILVCYsh8Dpo5yU+I76Y+nkjsZG7mGZz4Ceph4FUVMgntJZUGRVqJqLe97lZ5L5vpIZFteGf6eIVshARNMglQAZPlM0dZ8oBQTB1jwV1BFESRZHXNN7XeuetGok1IhfvN1y-OeqhuBFagANLVAAnPMADsABSbgADRGINBQnCkZj-Temh3nOD0ALLeGIRzg3V8wAGxMkIvGsBl2PpQFOMIKAABqxCEBpSX4ylWMU9T7AyisawbFsoBLCkKR8NcOGsuynI8TE9PrJs2wsykcQcwhe3EaIpFfoSAASojCJ+sqIpdPCpTTGvYHjmuQKA+C0IT4XkzT2s62b8AyntkhdlIwiBNcJhqDoQ1IttpUYli-RGKIXusSkBKOzoQgCjEVteksshSHu9uuztjH9Oitu9tcEQ0aVI4SwdMtuBxrN5-nKTysr0ojiyIdq1T5tV9ApvU6AeX4JQAMEM9+DEAA6mQGu19XVeW6NoByN4GT22oLuqKsZQcYMcEIWy4gcts30RaQ3pD5oGTXGo0rl9w-OM9sXgEp4qikkcnMrt6Zgc5d++C8zosn1hMhTwMMgX6AnhXxzvMV8bvdmx7hlUAgAiEBVIAfBAVR+hVIAAhBADEIIAVBBAA4IKAGBCDAAoIIABBBABYIIAQhB-5pSAQAzWvBK7EMAerNKoBAB4IOAsBSCoGYMAGQgKpAD0IKgzBgBEEGoSAwA1CCgHwdQzhgAqEAQdgkBmCGEELYEQ8h1M-4ajtIACRBzSgDtNWB0JoyyABEQGMaoSzNRyiJQg1FSpVQnMNHcHYjGHkUqVOqylVLWOEoeQoUcjilTcaCICGxxBiCcYJFxfZfFiHdlHMQQ0AktT7KDAAVvYoC1FrxmCAopa8hQgJeKODxMOcSgJxMCF1J+vx9JuGoJ4NQ6JPBuCZH9RiKS-h1TiJ9WpjEMl-CyaoX4IT1xuFljZBWzN-aiByD4JkrSRqEUkEROcGJdCpLXIUppAAOJk0RSGWitFGWM8ZTQWitO6B0WozRWldIAcRATRqmTHaaMxYSzui1BGFsWp3SoFyWYWJJ5bBFK6aU8plTqmtJHA034KygX4S6Z0v4PSHr9LnHbFmbJRk1ImagCyvYIoDIcuCL54RHKQlAHEuI3pPBVHRQSolV8yX4uBJSwojgw4zIkInZ4cRVCYrnNi0QuLQVrlUHECUayvJgAjOoq0Zo7QXKuTcu5Dynmug1FaN5A84nzEKYxYpfyKlVJqXU4FXSmktL1RCjp7iukwoKHCwZiKRmWBRXUgU6LQActEFy+YAAecg8FbDkFAKIAAHnuGQ1Q1B-AANplFEEcIaABdVQsglg-FwEyUAAB+PAoAvi2pUtwRlAy5nPHmOyrFjkPXktpR8+Y3rGl8viKs9Zf85F90oU27uLbW3yPbR2-Gsju24y7X2jKvbB2BWHdgUAgAKEHAZgmBgBkEEwYAbBBsGYNAOA6hoCIGMJYSqUAYVaAaVwAADVAOghd7CkEIMALgg7DfJjpHf5WIfANlOvwHscSZgxCeurcYRWVRyD2FABssOkcNieGEBFcQnho7fNiJVIGZTPA5GEP9WDqIHFvs-qG19itPAftEO65SsE6kYBHHIdEKRxApAeihjAAoMBOog1B11jlAjlq9FHKQYHGN7mCGAJ1eUBZbC5YECKEgoaiAEwfKoonxB5TXJJwW0QMBhzZNUBQPYrB-g0Ex9lkG9zCdsCBzj4G9OiABt2A+qgFNCccutbjzHLJu3+CRvq-Qo6FHRDcCzgtvTos5i5vCoBCgVN7NZwiXLvRRXIAAfXwHlGGtBlCocCyOeei8-jeky9pvcAN9Lea2GvCSn5ppCoC3hYLI8wtqAi1F6LeUADiiXkspcBGlzkGWsumfM7W-LPoh5HGK9UUrYAWvVFEF2KkSWystba9sbLURmssloxs3Iph66CfCyx5zI3US1biwlwIj1aAxf27QGjO3AS1Ya-QdwV3GtuFiHRsA2UgkyfE2F5EZX0VWY29VxykXjuxfi7QXjeF+uDciY9jZL3coybkxJjbn2Lt4vBD9g+f3wQA5i9d0HpGispGmiZBqObluxEZWJ+89tzGLjsxTucH23a3W++t9HNXAenYBuDgnF5qLo1B+T2Tiypq1uUrTwXCOpOM+6Y5NHgsMdSzu7QTn+PCefzXHzv+cRn0sew++z9XrAg-rtoYf9gGycDyM1x0z6qytaqQ+dvC6Hf0VL1m+3Dn7CPEeR2RijVGCgO6eyjjFpmDNB4JZbkzTHQf8d+6HsK3N7IsfdWxjI4hxPwUMPHhenJhPJ5peoCQ8PYLrOUwPVT6nfw3Hm7ppjoeI-2fMxt2Xb4uWxyR3hNQbnxAea8xt3ztn28tYq72Mke3gdkim8jmbCeOvenm7loGvXCsDe58NlrQWQugFH4D67E-mvT+z9sX4mW59dbV+uJfXOSu45S2Nib+xJ-r9SzP+bN+GKk9L1MgvafKd-mp7oNvJnGXAoMfBLcGK-Hne8FNF3K2aUT-d8VPeHe2dDYnQA6XVHEAnfe7ZXFfVXRSaA0NWApTMAeYbXSyXXd3fDA3WwI3P9ADIDC3GQDjK3JjG3ZHO3BWB3WqEXDDF3CgvDAjUyTQIjZrH3SjajL3UAQPBjEPFjWwNjevUzaPFjagLEcEbQUQeXL9QIX1ANINENcNSNaNHqHQONcOJNFNdNXATNb2UZZQyyKrOPBPLld1VQlIdQqQTQlwtjXrNQWCWCP+FTUQNTDTUeUzGvfTOQ9jUDSPHLT+JvFnOXVvWebbDvLvHvI6PvPzQfFLYfLfUA2gPfabFLWbWfefQGaGeIg+ZfQbNfIfTfbfbHRrIoqfEomfY-TrJjbrC-PvCAuo2-cbTwSbffNow-WkKPRbKHc3L-XwtgjvOndcW4IGaqNA5nI7E7YHcAlXSAucAgoLUaAQSQkcRA2tI6Hg1A2eIAjA9YurbAwebY0+dXTDfYqZQ4qHIAA
 
 // ----------------------------------------------------------------------------------------
 // -------------------------------------- UNION TYPE --------------------------------------
@@ -176,17 +176,95 @@ function bar(a: any) {
   a.b()
 }
 
-// function f2(a: unknown) {
-//   a.b();
-// }
+function f2(a: unknown) {
+  a.b();
+}
 
 
 
 
 
 // ----------------------------------------------------------------------------------------
-// ----------------------------------- ТИПИЗАИРУЕМ РЕДАКС ---------------------------------
+// ----------------------------------- ТИПИЗИРУЕМ РЕДАКС ----------------------------------
 // ----------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------
+// ------------------------------ ОПТИМИЗАЦИЯ РАБОТЫ С ОБЪЕКТАМИ --------------------------
+// ----------------------------------------------------------------------------------------
+
+
+// пишем интерфейсы
+interface Iname {
+    name: string;
+}
+
+interface Iage {
+    age: number;
+}
+
+interface Icity {
+    city: Ititle;
+}
+
+interface Ititle {
+    title: string;
+}
+
+interface Iobj {
+    a: Iname;
+    b: Iage;
+    c: Icity;
+}
+
+const obj: Iobj = {
+    a: {name: 'Pasha'},
+    b: {age: 27},
+    c: {city: {title: 'Homel forever'}},
+}
+
+const something: Iage = {age: 28};
+
+
+
+// задействуем автоматический вывод типов
+const obj2 = {
+    a: {name: 'Pasha'},
+    b: {age: 28},
+    c: {city: {title: 'Homel forever'}},
+}
+type SomeType2 = typeof obj2.a | typeof obj2.b | typeof obj2.c;
+const something2: SomeType2 = {age: 25};
+
+
+
+
+
+// динамический вывод типа
+const obj3 = {
+    a: {name: 'Pasha'},
+    b: {age: 27},
+    c: {city: {title: 'Homel forever'}},
+}
+
+type SomeType3<T> = T extends {[key: string]: infer U} ? U : never;
+
+const something3: SomeType3<typeof obj3> = {age: 28};
+
+
+
+
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// -------------------------- ШПАРГАЛКА ПО ТИПИЗАЦИИ REDUX ДЛЯ МЕНЯ -----------------------
+// ----------------------------------------------------------------------------------------
+
+// 1
 
 // type Nullable<T> = null | T; 
 
@@ -229,28 +307,62 @@ function bar(a: any) {
 // const setAge = (age: number): SetAgeAction => ({type: ActionsType.SET_AGE, payload: age});
 
 
-type Nullable<T> = null | T; 
+// 2
 
-const initialState = {
-    name: 'Pavel',
-    age: null as Nullable<number>,
-    photo: '',
-}
+// type Nullable<T> = null | T; 
 
-type StateType = typeof initialState;
-type ActionsType = ReturnType <typeof setName> | ReturnType <typeof setAge>;
+// const initialState = {
+//     name: 'Pavel',
+//     age: null as Nullable<number>,
+//     photo: '',
+// }
+
+// type StateType = typeof initialState;
+// type ActionsType = ReturnType <typeof setName> | ReturnType <typeof setAge>;
 
 
-const reducer = (state: StateType = initialState, action: ActionsType) => {
-    switch (action.type) {
-        case "SET_NAME":
-            return {...state, name: action.payload};
-        case "SET_AGE":
-            return {...state, age: action.payload};
-        default:
-            return state;
-    }
-}
+// const reducer = (state: StateType = initialState, action: ActionsType) => {
+//     switch (action.type) {
+//         case "SET_NAME":
+//             return {...state, name: action.payload};
+//         case "SET_AGE":
+//             return {...state, age: action.payload};
+//         default:
+//             return state;
+//     }
+// }
 
-const setName = (name: string) => ({type: 'SET_NAME', payload: name} as const);
-const setAge = (age: number) => ({type: 'SET_AGE', payload: age} as const);
+// const setName = (name: string) => ({type: 'SET_NAME', payload: name} as const);
+// const setAge = (age: number) => ({type: 'SET_AGE', payload: age} as const);
+
+// 3
+
+// type Nullable<T> = null | T; 
+
+// const initialState = {
+//     name: 'Pavel',
+//     age: null as Nullable<number>,
+//     photo: '',
+// }
+
+// type StateType = typeof initialState;
+// type ProperiesType<T> = T extends {[key: string]: infer U} ? U : never;
+// type ActionsType = ReturnType<ProperiesType<typeof actions>>
+
+
+// const reducer = (state: StateType = initialState, action: ActionsType) => {
+//     switch (action.type) {
+//         case "SET_NAME":
+//             return {...state, name: action.payload};
+//         case "SET_AGE":
+//             return {...state, age: action.payload};
+//         default:
+//             return state;
+//     }
+// }
+
+// const actions = {
+//     setName: (name: string) => ({type: 'SET_NAME', payload: name} as const),
+//     setAge: (age: number) => ({type: 'SET_AGE', payload: age} as const),
+// }
+
